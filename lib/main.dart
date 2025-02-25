66d1e84650dfa9fdc946734d027b2a930b0704da
ELIZIEL_CAMPEROS_GARCIA <a.22308051281159@cbtis128.edu.mx>
2025-02-25 17:06:27 +0000

    ima

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
                       color: Colors.blueGrey)),
               SizedBox(height: 10),
               Image.network(
-                'https://private-user-images.githubusercontent.com/179508220/416763808-ce597651-4b0b-49be-bc8c-dc3f6fd61e19.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDA1MDAyMDcsIm5iZiI6MTc0MDQ5OTkwNywicGF0aCI6Ii8xNzk1MDgyMjAvNDE2NzYzODA4LWNlNTk3NjUxLTRiMGItNDliZS1iYzhjLWRjM2Y2ZmQ2MWUxOS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjI1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIyNVQxNjExNDdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yZGFlMjAwODZhZTQyMjUzYTRmZjcxNGUyMzEzMzAzNWQyOGZjZjE2NDg5NjZiOWE1ZTg2OWRlODg2NWM5ZjBiJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.ebXiDLWLvYODYWNVMsRCkghS6V9GFvLWaT5aq02zrXc',
+                'https://raw.githubusercontent.com/Eliziel-Camperos/imagenesAPPflutter_6J/refs/heads/main/spiderpapu3.png',
                 height: 180,
                 fit: BoxFit.cover,
               ),
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
                       color: Colors.black87)),
               SizedBox(height: 10), // Espacio entre imágenes
               Image.network(
-                'https://private-user-images.githubusercontent.com/179508220/416765966-4f350771-4916-4357-ba5f-ced690072ed7.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDA1MDAyMDcsIm5iZiI6MTc0MDQ5OTkwNywicGF0aCI6Ii8xNzk1MDgyMjAvNDE2NzY1OTY2LTRmMzUwNzcxLTQ5MTYtNDM1Ny1iYTVmLWNlZDY5MDA3MmVkNy5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjI1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIyNVQxNjExNDdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT00ODQ0NjUwMTlmYzk0YjI3YjM1YzdkNjRmNmVjYjI3ODUwNzJhN2RjZmVlOWZmYTkyNGFhMmMwNmM4OTdmOTFiJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.zaTmUSCperqJzFZg9seFxu8yj7h3I8uZ8dHqkc-HgsM',
+                'https://raw.githubusercontent.com/Eliziel-Camperos/imagenesAPPflutter_6J/refs/heads/main/spiderpapu2.png',
                 height: 180,
                 fit: BoxFit.cover,
               ),
@@ -53,7 +53,7 @@ class MyApp extends StatelessWidget {
                       color: Colors.black87)),
               SizedBox(height: 10),
               Image.network(
-                'https://private-user-images.githubusercontent.com/179508220/416766085-646a04e4-c683-494e-addf-17592b286914.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDA1MDAyMDcsIm5iZiI6MTc0MDQ5OTkwNywicGF0aCI6Ii8xNzk1MDgyMjAvNDE2NzY2MDg1LTY0NmEwNGU0LWM2ODMtNDk0ZS1hZGRmLTE3NTkyYjI4NjkxNC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjI1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIyNVQxNjExNDdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1lOTMzNWE0ZDlhYjNkNGJlZjAyNTc5NmI3NGM3YzU0MmQ3MjIyMGRhNmZhMGEwMDZlMjIyNzkxMDJkYjg3ZmFiJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.QK2F7qWW9_oRMfMKbKNZDquklieMyUz9eB7CGIMrhnE',
+                'https://raw.githubusercontent.com/Eliziel-Camperos/imagenesAPPflutter_6J/refs/heads/main/spiderpapu1.png',
                 height: 180,
                 fit: BoxFit.cover,
               ),
